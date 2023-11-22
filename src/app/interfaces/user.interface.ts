1832b7d8c70e20c9fdff114dc5d8550ceec3c26e
islammazharul <islammazharul3645@gmail.com>
2023-11-22 23:55:50 +0600

    make a user interface in this project

---
+++ src/app/interfaces/user.interface.ts
@@ -0,0 +1,20 @@
+
+interface User {
+    userId: string;
+    username: string;
+    password: string;
+    fullName: {
+        firstName: string;
+        lastName: string;
+    },
+    age: number;
+    email: string;
+    isActive: boolean;
+    hobbies: string[];
+    address: {
+        street: string;
+        city: string;
+        country: string
+    }
+}
+export { User }
