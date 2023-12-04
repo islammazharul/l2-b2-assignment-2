05e839eb25b5b47cb00d087285509845d879a945
islammazharul <islammazharul3645@gmail.com>
2023-12-05 00:45:45 +0600

    remove some comment

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,163 +3,161 @@ import { userServices } from './user.service';
 import userSchema from './user.validation';
 
 const createUser = async (req: Request, res: Response) => {
-    try {
-        const { user: userData } = req.body;
-        console.log(userData);
-        // data validation using zod package
-        const parseDataByZod = userSchema.parse(userData);
-        const result = await userServices.createUserIntoDb(parseDataByZod);
-        // console.log(result);
-        res.status(200).json({
-            success: true,
-            message: 'User created successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const { user: userData } = req.body;
+    // data validation using zod package
+    const parseDataByZod = userSchema.parse(userData);
+    const result = await userServices.createUserIntoDb(parseDataByZod);
+    res.status(200).json({
+      success: true,
+      message: 'User created successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const getAllUser = async (req: Request, res: Response) => {
-    try {
-        const result = await userServices.getAllUserFromDb();
-        res.status(200).json({
-            success: true,
-            message: 'Users fetched successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const result = await userServices.getAllUserFromDb();
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const getSingleUser = async (req: Request, res: Response) => {
-    try {
-        const id = parseInt(req.params.userId);
-        const result = await userServices.getSingleUserFromDb(id);
-        res.status(200).json({
-            success: true,
-            message: 'Users fetched successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const id = parseInt(req.params.userId);
+    const result = await userServices.getSingleUserFromDb(id);
+    res.status(200).json({
+      success: true,
+      message: 'Users fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const updateUser = async (req: Request, res: Response) => {
-    try {
-        const updateData = req.body;
-        const id = parseInt(req.params.userId);
-        const result = await userServices.updateUserInDb(id, updateData);
-        res.status(200).json({
-            success: true,
-            message: 'User updated successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const updateData = req.body;
+    const id = parseInt(req.params.userId);
+    const result = await userServices.updateUserInDb(id, updateData);
+    res.status(200).json({
+      success: true,
+      message: 'User updated successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 const addOrderCollection = async (
-    req: Request,
-    res: Response,
+  req: Request,
+  res: Response,
 ): Promise<void> => {
-    try {
-        const { userId } = req.params;
-        const orderProduct = req.body;
-
-        // console.log(orderProduct, userId);
-        const result = await userServices.updateUserInDb(Number(userId), orderProduct);
-        // console.log(result);
-        res.status(200).json({
-            success: true,
-            message: 'Order created successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const { userId } = req.params;
+    const orderProduct = req.body;
+    const result = await userServices.updateUserInDb(
+      Number(userId),
+      orderProduct,
+    );
+    res.status(200).json({
+      success: true,
+      message: 'Order created successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const deleteUser = async (req: Request, res: Response) => {
-    try {
-        const userId = parseInt(req.params.userId);
-        const result = await userServices.deleteUserFromDb(userId);
-        res.status(200).json({
-            success: true,
-            message: 'User deleted successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const userId = parseInt(req.params.userId);
+    await userServices.deleteUserFromDb(userId);
+    res.status(200).json({
+      success: true,
+      message: 'User deleted successfully!',
+      data: null,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const singleUserOrder = async (req: Request, res: Response) => {
-    try {
-        const id = parseInt(req.params.userId);
-        const result = await userServices.singleUserOrderDb(id);
-        res.status(200).json({
-            success: true,
-            message: 'Order fetched successfully!',
-            data: result,
-        });
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-        res.status(400).json({
-            success: false,
-            message: error.message || 'Something went wrong!',
-            data: error,
-        });
-    }
+  try {
+    const id = parseInt(req.params.userId);
+    const result = await userServices.singleUserOrderDb(id);
+    res.status(200).json({
+      success: true,
+      message: 'Order fetched successfully!',
+      data: result,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    res.status(400).json({
+      success: false,
+      message: error.message || 'Something went wrong!',
+      data: error,
+    });
+  }
 };
 
 const totalPrice = async (req: Request, res: Response) => {
-    const userId = parseInt(req.params.userId);
-    const result = await userServices.totalPriceOfOrder(userId);
-    res.json({
-        data: result,
-    });
+  const userId = parseInt(req.params.userId);
+  const result = await userServices.totalPriceOfOrder(userId);
+  res.json({
+    data: result,
+  });
 };
 
 export const userController = {
-    createUser,
-    getAllUser,
-    getSingleUser,
-    updateUser,
-    deleteUser,
-    addOrderCollection,
-    singleUserOrder,
-    totalPrice,
+  createUser,
+  getAllUser,
+  getSingleUser,
+  updateUser,
+  deleteUser,
+  addOrderCollection,
+  singleUserOrder,
+  totalPrice,
 };
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,182 +2,182 @@ import User from '../user.model';
 import { TOrderProduct, TUser } from './user.interface';
 
 const createUserIntoDb = async (userData: TUser) => {
-    // instance method
-    const user = new User(userData); /* create an instance */
-    if (await user.isUserExist(userData.userId)) {
-        throw new Error('User already exists!');
-    }
-    const result = user.save();
-    return result;
+  // instance method
+  const user = new User(userData); /* create an instance */
+  if (await user.isUserExist(userData.userId)) {
+    throw new Error('User already exists!');
+  }
+  const result = user.save();
+  return result;
 };
 
 const getAllUserFromDb = async () => {
-    const result = await User.aggregate([
-        {
-            $project: {
-                username: 1,
-                fullName: 1,
-                age: 1,
-                email: 1,
-                address: 1,
-            },
-        },
-    ]);
-    return result;
+  const result = await User.aggregate([
+    {
+      $project: {
+        username: 1,
+        fullName: 1,
+        age: 1,
+        email: 1,
+        address: 1,
+      },
+    },
+  ]);
+  return result;
 };
 
 const getSingleUserFromDb = async (id: number) => {
-    const userID = id;
-    const user = new User({ userId: id });
-    const doesExist = await user.isUserExist(userID);
-    if (!doesExist) {
-        throw new Error('User does not exist!');
-    } else {
-        const result = await User.aggregate([
-            { $match: { userId: id } },
-            {
-                $project: {
-                    userId: 1,
-                    username: 1,
-                    fullName: 1,
-                    age: 1,
-                    email: 1,
-                    isActive: 1,
-                    hobbies: 1,
-                    address: 1,
-                },
-            },
-        ]);
-        return result;
-    }
+  const userID = id;
+  const user = new User({ userId: id });
+  const doesExist = await user.isUserExist(userID);
+  if (!doesExist) {
+    throw new Error('User does not exist!');
+  } else {
+    const result = await User.aggregate([
+      { $match: { userId: id } },
+      {
+        $project: {
+          userId: 1,
+          username: 1,
+          fullName: 1,
+          age: 1,
+          email: 1,
+          isActive: 1,
+          hobbies: 1,
+          address: 1,
+        },
+      },
+    ]);
+    return result;
+  }
 };
 
 const updateUserInDb = async (
-    id: number,
-    updateData: TUser,
+  id: number,
+  updateData: TUser,
 ): Promise<TUser | null> => {
-    const userID = id;
-    const user = new User({ userId: id });
-    const doesExist = await user.isUserExist(userID);
-    if (!doesExist) {
-        throw new Error('User does not exist!');
-    } else {
-        const result = await User.findOneAndUpdate({ userId: id }, updateData, {
-            new: true,
-            runValidators: true,
-        });
-        return result;
-    }
+  const userID = id;
+  const user = new User({ userId: id });
+  const doesExist = await user.isUserExist(userID);
+  if (!doesExist) {
+    throw new Error('User does not exist!');
+  } else {
+    const result = await User.findOneAndUpdate({ userId: id }, updateData, {
+      new: true,
+      runValidators: true,
+    });
+    return result;
+  }
 };
 
 const deleteUserFromDb = async (id: number) => {
-    const user = new User({ userId: id });
-    const userID = id;
+  const user = new User({ userId: id });
+  const userID = id;
 
-    const doesExist = await user.isUserExist(userID);
-    if (!doesExist) {
-        throw new Error('User does not exist!');
-    } else {
-        const result = await User.findOneAndDelete({ userId: id });
-        return result;
-    }
+  const doesExist = await user.isUserExist(userID);
+  if (!doesExist) {
+    throw new Error('User does not exist!');
+  } else {
+    const result = await User.findOneAndDelete({ userId: id });
+    return result;
+  }
 };
 
 const addOrderCollectionInDb = async (
-    id: number,
-    orderProduct: TOrderProduct,
+  id: number,
+  orderProduct: TOrderProduct,
 ) => {
-    const userID = id;
-    const user = new User({ userId: id });
+  const userID = id;
+  const user = new User({ userId: id });
 
-    const doesExist = await user.isUserExist(userID)
-    if (!doesExist) {
-        throw new Error('User does not exist!');
-    } else {
-        const result = await User.findOneAndUpdate(
-            { userId: id },
-            { $push: { orders: orderProduct } },
-            { new: true, upsert: true }
-        );
-        return result;
-    }
+  const doesExist = await user.isUserExist(userID);
+  if (!doesExist) {
+    throw new Error('User does not exist!');
+  } else {
+    const result = await User.findOneAndUpdate(
+      { userId: id },
+      { $push: { orders: orderProduct } },
+      { new: true, upsert: true },
+    );
+    return result;
+  }
 };
 
 const singleUserOrderDb = async (id: number) => {
-    const userID = id;
-    const user = new User({ userId: id });
-    const doesExist = await user.isUserExist(userID);
-    if (!doesExist) {
-        throw new Error('User does not exist!');
-    } else {
-        const result = await User.aggregate([
-            { $match: { userId: id } },
-            {
-                $project: {
-                    orders: 1,
-                },
-            },
-        ]);
-        return result;
-    }
+  const userID = id;
+  const user = new User({ userId: id });
+  const doesExist = await user.isUserExist(userID);
+  if (!doesExist) {
+    throw new Error('User does not exist!');
+  } else {
+    const result = await User.aggregate([
+      { $match: { userId: id } },
+      {
+        $project: {
+          orders: 1,
+        },
+      },
+    ]);
+    return result;
+  }
 };
 
 const totalPriceOfOrder = async (id: number) => {
-    const userID = id;
-    const user = new User({ userId: id });
-    const doesExist = await user.isUserExist(userID);
-    if (doesExist) {
-        const result = await User.aggregate([
-            { $match: { userId: id } },
-            {
-                $unwind: '$orders',
+  const userID = id;
+  const user = new User({ userId: id });
+  const doesExist = await user.isUserExist(userID);
+  if (doesExist) {
+    const result = await User.aggregate([
+      { $match: { userId: id } },
+      {
+        $unwind: '$orders',
+      },
+      {
+        $group: {
+          _id: '$userId',
+          totalPrice: {
+            $sum: {
+              $multiply: ['$orders.price', '$orders.quantity'],
             },
-            {
-                $group: {
-                    _id: '$userId',
-                    totalPrice: {
-                        $sum: {
-                            $multiply: ['$orders.price', '$orders.quantity'],
-                        },
-                    },
-                },
-            },
-            {
-                $project: {
-                    totalPrice: { $round: ['$totalPrice', 2] },
-                },
-            },
-        ]);
-        if (result.length > 0) {
-            const response = {
-                success: true,
-                message: 'Total price calculated successfully!',
-                data: {
-                    totalPrice: result[0].totalPrice,
-                },
-            };
-            return response;
-        } else {
-            const errorResponse = {
-                success: false,
-                message: 'User not found',
-                error: {
-                    code: 404,
-                    description: 'User not found!',
-                },
-            };
-            return errorResponse;
-        }
+          },
+        },
+      },
+      {
+        $project: {
+          totalPrice: { $round: ['$totalPrice', 2] },
+        },
+      },
+    ]);
+    if (result.length > 0) {
+      const response = {
+        success: true,
+        message: 'Total price calculated successfully!',
+        data: {
+          totalPrice: result[0].totalPrice,
+        },
+      };
+      return response;
+    } else {
+      const errorResponse = {
+        success: false,
+        message: 'User not found',
+        error: {
+          code: 404,
+          description: 'User not found!',
+        },
+      };
+      return errorResponse;
     }
+  }
 };
 
 export const userServices = {
-    createUserIntoDb,
-    getAllUserFromDb,
-    getSingleUserFromDb,
-    updateUserInDb,
-    deleteUserFromDb,
-    addOrderCollectionInDb,
-    singleUserOrderDb,
-    totalPriceOfOrder,
+  createUserIntoDb,
+  getAllUserFromDb,
+  getSingleUserFromDb,
+  updateUserInDb,
+  deleteUserFromDb,
+  addOrderCollectionInDb,
+  singleUserOrderDb,
+  totalPriceOfOrder,
 };
